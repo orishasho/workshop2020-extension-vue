e08b72415358f2e37ec9f9bddd7ac22bbf28e2e1
oshasho <oshasho@tremorvideo.com>
2020-04-05 08:22:59 +0300

    refactored isViewingCoursesTable function with jQuery

--- src/modules/viewing-states.js
+++ src/modules/viewing-states.js
@@ -1,37 +1,12 @@
+import $ from 'jquery'
+
 export function isViewingGrades() {
     return false;
 }
 
 export function isViewingCoursesTable() {
-    const h1PullRightElements = document.querySelectorAll("h1.pull-right");
-    const h2PanelTitleElements = document.querySelectorAll("h2.panel-title");
-
-    const h1AdvancedSearchElementsText = "חיפוש מתקדם";
-    const h2AdvancedSearchElementsText = "רשימת אתרי קורסים"
-
-    const h1StudyProgramElementsText = "חיפוש קורסים";
-    const h2StudyProgramElementsText = "רשימת קורסים לפי התחום המבוקש";
-
-    let hasFirstHeading = false;
-    let hasSecondHeading = false;
-
-    for (let i = 0; i < h1PullRightElements.length; i++) {
-        let element = h1PullRightElements[i];
-        if (element.innerHTML.indexOf(h1AdvancedSearchElementsText) !== -1 || element.innerHTML.indexOf(h1StudyProgramElementsText) !== -1) {
-            hasFirstHeading = true;
-            break;
-        }
+    const firstHeadingElements = $("h1.pull-right:contains('חיפוש מתקדם'), h1.pull-right:contains('חיפוש קורסים')");
+    if (firstHeadingElements.length > 0) {
+        return $("h2.panel-title:contains('רשימת אתרי קורסים'), h2.panel-title:contains('רשימת קורסים לפי התחום המבוקש')").length > 0;
     }
-
-    if (hasFirstHeading) {
-        for (let i = 0; i < h2PanelTitleElements.length; i++) {
-            let element = h2PanelTitleElements[i];
-            if (element.innerHTML.indexOf(h2AdvancedSearchElementsText) !== -1 || element.innerHTML.indexOf(h2StudyProgramElementsText) !== -1) {
-                hasSecondHeading = true;
-                break;
-            }
-        }
-    }
-
-    return hasFirstHeading && hasSecondHeading;
 }
