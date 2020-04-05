057a2d7a0ca5ed66c180ea0230dbb849a7265f71
oshasho <oshasho@tremorvideo.com>
2020-04-05 08:34:43 +0300

    added comment

--- src/modules/viewing-states.js
+++ src/modules/viewing-states.js
@@ -5,6 +5,7 @@ export function isViewingGrades() {
 }
 
 export function isViewingCoursesTable() {
+    // return true if DOM contains one of the first headings and one of the second headings
     const firstHeadingElements = $("h1.pull-right:contains('חיפוש מתקדם'), h1.pull-right:contains('חיפוש קורסים')");
     if (firstHeadingElements.length > 0) {
         return $("h2.panel-title:contains('רשימת אתרי קורסים'), h2.panel-title:contains('רשימת קורסים לפי התחום המבוקש')").length > 0;
