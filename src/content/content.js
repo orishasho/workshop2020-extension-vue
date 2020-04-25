50e852c7ef774f3b25db7623ff4ed8c23de43ea7
YuvalsTwingo <sedler@twingo.co.il>
2020-04-25 13:07:02 +0300

    added extension tab to meida net menu

--- src/content/content.js
+++ src/content/content.js
@@ -10,3 +10,28 @@ if (viewingStatesModule.isViewingCoursesTable()) {
 } else if (viewingStatesModule.isViewingGrades()) {
     fetchUserCoursesModule.fetchUserCoursesInfo();
 }
+
+let navigatorMenu = document.querySelector(".navbar-nav");
+let navigatorMenuItem = navigatorMenu.querySelector(".dropdown");
+let newNavigatorMenuItem = navigatorMenuItem.cloneNode(true);
+newNavigatorMenuItem.firstChild.textContent = "לשונית תוסף";
+
+newNavigatorMenuItem.querySelectorAll("li").forEach(function(a) {
+    a.remove();
+})
+
+let newNavigatorMenuItemDropDown = newNavigatorMenuItem.querySelector(".dropdown-menu");
+
+function addItemToNewTabDropDown(dropDownList, itemText, itemLink) {
+    let listItem = document.createElement("li");
+    let listItemLink = document.createElement("a");
+    listItemLink.setAttribute('target', '_blank');
+    listItemLink.setAttribute('href', itemLink);
+    listItemLink.textContent = itemText;
+    listItem.appendChild(listItemLink);
+    dropDownList.appendChild(listItem);
+}
+
+addItemToNewTabDropDown(newNavigatorMenuItemDropDown, "כניסה לפייסבוק", "http://www.facebook.com");
+addItemToNewTabDropDown(newNavigatorMenuItemDropDown, "כניסה לגוגל", "http://www.google.com");
+navigatorMenu.appendChild(newNavigatorMenuItem);
