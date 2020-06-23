0511c442551fdebb7286faf82acd22efb7cf4f9c
oshasho <oshasho@tremorvideo.com>
2020-06-23 15:22:34 +0300

    WIP: new design for extension dropdown tab

--- src/components/ExtensionTab/ExtensionDropdown.vue
+++ src/components/ExtensionTab/ExtensionDropdown.vue
@@ -1,5 +1,5 @@
 <template>
-    <li class="dropdown">
+    <li class="dropdown extension-dropdown">
         <a href="#" class="dropdown-toggle menu-item-has-children DDLocal" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">לשונית תוסף</a>
         <ul class="dropdown-menu">
             <ExtensionDropdownLinkClick :linkText="linkText"/>
@@ -22,6 +22,15 @@
     }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+    .extension-dropdown {
+        background: repeating-linear-gradient(
+                        45deg,
+                        #FFFFFF,
+                        #FFFFFF 10px,
+                        #ffcb3b 10px,
+                        #ffcb3b 20px
+        );
+        border-radius: 25px !important;
+    }
 </style>
