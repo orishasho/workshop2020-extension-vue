c7c20f8a56b24a7bc855b34c408c61987c6bd6b1
YuvalsTwingo <sedler@twingo.co.il>
2020-08-10 18:45:09 +0300

    added studd to schedule

--- src/loaders/courses-schedules-loader.js
+++ src/loaders/courses-schedules-loader.js
@@ -2,6 +2,8 @@ const axios = require('axios');
 const possibleCoursesApiUrl = 'http://localhost:8080/course_schedule/possible_courses';
 const allCoursesSchedulesApiUrl = 'http://localhost:8080/course_schedule/all';
 
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
 export default class CoursesSchedulesLoader {
 
     async getLoggedInUserIdFromChromeStorage() {
@@ -31,15 +33,20 @@ export default class CoursesSchedulesLoader {
 
     async getCoursesSchedules(courses_numbers, semester) {
         try {
-            //const response = await axios.get(allCoursesSchedulesApiUrl, { courses_numbers });
-            const response = await axios({
-                method: 'get',
-                url: allCoursesSchedulesApiUrl,
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                params: { arr: courses_numbers, semester: semester }
-            });
+            const response = await axios.post(
+                allCoursesSchedulesApiUrl, {
+                    arr: courses_numbers,
+                    semester: semester
+                }
+            );
+            // const response = await axios({
+            //     method: 'get',
+            //     url: allCoursesSchedulesApiUrl,
+            //     headers: {
+            //         'Content-Type': 'application/json'
+            //     },
+            //     params: { arr: courses_numbers, semester: semester }
+            // });
             const coursesSchedules = response.data;
             return coursesSchedules;
         } catch (e) {
