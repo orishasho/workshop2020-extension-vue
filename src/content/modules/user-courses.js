f1f23fb0c78c86f713a63b894de3513edced6923
oshasho <oshasho@tremorvideo.com>
2020-09-04 19:00:50 +0300

    fixed archi bug and some refactoring

--- src/content/modules/user-courses.js
+++ src/content/modules/user-courses.js
@@ -36,79 +36,86 @@ async function handleUserCourses(userCourses) {
 
 function filterCoursesManagementCollege(userCourses) {
     // 1. Keep only final grades
-    let filteredUserCourses = userCourses
+    let filteredUserCoursesWithPossibleDuplicates = userCourses
         .filter(userCourse => userCourse['סוג מקצוע'].includes('שעור') || userCourse['סוג מקצוע'].includes('פרויקטים'));
 
     // 2. Handle duplicates
-    for (let i = 0; i < filteredUserCourses.length; i++) {
-        // 2.1. Build duplicates array
-        let duplicates = [];
-
-        for (let j = 0; j < filteredUserCourses.length; j++) {
-            if (filteredUserCourses[i]['שם קורס'] === filteredUserCourses[j]['שם קורס'] && j !== i) {
-                duplicates.push(filteredUserCourses[j]);
-            }
-        }
-
-        if (duplicates.length > 0 &&
-            isNaN(filteredUserCourses[i]['ציון']) &&
-            filteredUserCourses[i]['ציון'] !== 'טרם') {
-            // 2.2. Course without grade, student is not enrolled to it again and there are duplicates - remove
-            filteredUserCourses.splice(i, 1);
-        } else if (duplicates.length > 0) {
-            // 2.3. Course with grade and there are duplicates - remove if it's not highest
-            const keepCourse = duplicates
-                .every(duplicate => isNaN(duplicate['ציון']) || duplicate['ציון'] < filteredUserCourses[i]['ציון']);
-            if (!keepCourse) {
-                filteredUserCourses.splice(i, 1);
-            }
-        }
-    }
+    const filteredUserCourses = filterDuplicateCourses(filteredUserCoursesWithPossibleDuplicates);
 
     return filteredUserCourses;
 }
 
 function filterCourses(userCourses) {
     // 1. Keep only final grades
-    let filteredUserCourses = userCourses
+    let filteredUserCoursesWithPossibleDuplicates = userCourses
         .filter(userCourse => userCourse['סוג ציון'] === 'שיעור/ציון קורס' && userCourse['שנה'] !== 'ללא שנה');
 
     // 2. Handle duplicates
-    for (let i = 0; i < filteredUserCourses.length; i++) {
-        // 2.1. Build duplicates array
+    const filteredUserCourses = filterDuplicateCourses(filteredUserCoursesWithPossibleDuplicates);
+
+    return filteredUserCourses;
+}
+
+function filterDuplicateCourses(filteredUserCoursesWithPossibleDuplicates) {
+    for (let i = 0; i < filteredUserCoursesWithPossibleDuplicates.length; i++) {
+        // 1. Build duplicates array
         let duplicates = [];
 
-        for (let j = 0; j < filteredUserCourses.length; j++) {
-            if (filteredUserCourses[i]['שם קורס'] === filteredUserCourses[j]['שם קורס'] && j !== i) {
-                duplicates.push(filteredUserCourses[j]);
+        for (let j = 0; j < filteredUserCoursesWithPossibleDuplicates.length; j++) {
+            if (filteredUserCoursesWithPossibleDuplicates[i]['שם קורס'] === filteredUserCoursesWithPossibleDuplicates[j]['שם קורס'] && j !== i) {
+                duplicates.push(filteredUserCoursesWithPossibleDuplicates[j]);
             }
         }
 
         if (duplicates.length > 0 &&
-            isNaN(filteredUserCourses[i]['ציון']) &&
-            filteredUserCourses[i]['ציון'] !== 'טרם') {
-            // 2.2. Course without grade, student is not enrolled to it again and there are duplicates - remove
-            filteredUserCourses.splice(i, 1);
+            isNaN(filteredUserCoursesWithPossibleDuplicates[i]['ציון']) &&
+            filteredUserCoursesWithPossibleDuplicates[i]['ציון'] !== 'טרם') {
+            // 2. Course without grade, student is not enrolled to it again and there are duplicates - remove
+            filteredUserCoursesWithPossibleDuplicates.splice(i, 1);
         } else if (duplicates.length > 0) {
-            // 2.3. Course with grade and there are duplicates - remove if it's not highest
+            // 3. Course with grade and there are duplicates - remove if it's not highest
             const keepCourse = duplicates
-                .every(duplicate => isNaN(duplicate['ציון']) || duplicate['ציון'] < filteredUserCourses[i]['ציון']);
+                .every(duplicate => isNaN(duplicate['ציון']) || duplicate['ציון'] < filteredUserCoursesWithPossibleDuplicates[i]['ציון']);
             if (!keepCourse) {
-                filteredUserCourses.splice(i, 1);
+                filteredUserCoursesWithPossibleDuplicates.splice(i, 1);
             }
         }
     }
 
-    return filteredUserCourses;
+    return filteredUserCoursesWithPossibleDuplicates;
 }
 
 
 
 async function storeUserCourses(userCourses) {
     // 1. Build user courses array to send to API
-    const apiUserCourses = [];
     const userId = await getLoggedInUserIdFromChromeStorage();
+    let apiUserCourses = buildUserCoursesForApi(userCourses, userId);
 
+    // 2. Send user courses array to API
+    // 2.1. Determine which API to use - MTA or Management College
+    const actualUserCourseApiUrl = getActualUserCourseApiUrl();
+
+    // 2.2. Send data
+    try {
+        await axios.post(
+            `${actualUserCourseApiUrl}/bulk`,
+            apiUserCourses
+        );
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function buildUserCoursesForApi(userCourses, userId) {
+    // 1. Build user courses array for API
+    let apiUserCourses = buildApiUserCoursesArrayWithPossibleDuplicates(userCourses, userId);
+    // 2. Cleanup - if there are 2 identical courses, one failed one signed, keep signed only
+    return cleanupFailedAndSignedCourses(apiUserCourses);
+}
+
+function buildApiUserCoursesArrayWithPossibleDuplicates(userCourses, userId) {
+    let apiUserCourses = [];
     userCourses.forEach(userCourse => {
         let apiUserCourse = {};
         const courseGrade = isNaN(userCourse['ציון']) ? -1 : userCourse['ציון'];
@@ -121,19 +128,45 @@ async function storeUserCourses(userCourses) {
         apiUserCourses.push(apiUserCourse);
     });
 
-    // 2. Send user courses array to API
+    return apiUserCourses;
+}
 
-    // 2.1. Determine which API to use - MTA or Management College
-    const actualUserCourseApiUrl = getActualUserCourseApiUrl();
+function cleanupFailedAndSignedCourses(userCoursesArray) {
+    // 1. Sort userCoursesArray by courseNumber asc
+    userCoursesArray.sort(sortUserCoursesByCourseNumberAsc);
+    // 2. Construct array of duplicates' courseNumbers
+    let courseNumbersWithDuplicates = buildCourseNumbersWithDuplicatesArray(userCoursesArray);
+    // 3. For each duplicate keep only 'signed'
+    const filteredArray = userCoursesArray.filter(userCourse =>
+        !courseNumbersWithDuplicates.find(courseNumber => courseNumber === userCourse.courseNumber) || userCourse.courseStatus === 'signed');
 
-    // 2.2. Send data
-    try {
-        await axios.post(
-            `${actualUserCourseApiUrl}/bulk`,
-            apiUserCourses
-        );
-    } catch (error) {
-        console.log(error);
+    return filteredArray;
+}
+
+function buildCourseNumbersWithDuplicatesArray(userCoursesArray) {
+    let courseNumbersWithDuplicates = [];
+    let prevCourseNumber = null;
+    let currentCourseNumber;
+    userCoursesArray.forEach((currentUserCourse, index, array) => {
+        if (index > 0) {
+            currentCourseNumber = currentUserCourse.courseNumber;
+            if (currentCourseNumber === prevCourseNumber &&
+                !courseNumbersWithDuplicates.find(courseNumber => courseNumber === currentCourseNumber)) {
+                courseNumbersWithDuplicates.push(currentCourseNumber);
+            }
+        }
+        prevCourseNumber = array[index].courseNumber;
+    });
+    return courseNumbersWithDuplicates;
+}
+
+function sortUserCoursesByCourseNumberAsc(userCourse1, userCourse2) {
+    if (userCourse1.courseNumber < userCourse2.courseNumber) {
+        return - 1;
+    } else if (userCourse1.courseNumber > userCourse2.courseNumber) {
+        return 1;
+    } else {
+        return 0;
     }
 }
 
